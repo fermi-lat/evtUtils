a8e2683eca69156cdaca68e27c89351969659aa9
heather <>
2010-09-14 16:38:48 +0000

    patch evtUtilsLib.py to include ROOT libs

--- evtUtilsLib.py
+++ evtUtilsLib.py
@@ -1,9 +1,12 @@
-#$Id: evtUtilsLib.py,v 1.2 2008/02/26 03:10:49 glastrm Exp $
+#$Id: evtUtilsLib.py,v 1.1 2010/06/16 20:44:41 jchiang Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['evtUtils'])
     env.Tool('facilitiesLib')
     env.Tool('xmlBaseLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
+    env.Tool('addLibrary', library = env['rootGuiLibs'])
+
 
 def exists(env):
     return 1
