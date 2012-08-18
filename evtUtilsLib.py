5838455db27089bd4bb1fc91e3ee92035ac22f6c
jrb <>
2012-08-18 00:25:52 +0000

    support no-install headers on Windows

--- evtUtilsLib.py
+++ evtUtilsLib.py
@@ -1,7 +1,9 @@
-#$Id: evtUtilsLib.py,v 1.1 2010/06/16 20:44:41 jchiang Exp $
+#$Id: evtUtilsLib.py,v 1.2 2010/09/14 16:38:48 heather Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['evtUtils'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'evtUtils') 
     env.Tool('facilitiesLib')
     env.Tool('xmlBaseLib')
     env.Tool('addLibrary', library = env['rootLibs'])
