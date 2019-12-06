8fc214e053f999e7345721db81254a526e2950e1
Tom Stephens <thomas.stephens@gmail.com>
2019-12-06 11:32:02 -0700

    Fixed tabs and indention issues

--- evtUtilsLib.py
+++ evtUtilsLib.py
@@ -3,7 +3,7 @@ def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['evtUtils'])
         if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
-	    env.Tool('findPkgPath', package = 'evtUtils') 
+            env.Tool('findPkgPath', package = 'evtUtils') 
     env.Tool('facilitiesLib')
     env.Tool('xmlBaseLib')
     env.Tool('addLibrary', library = env['rootLibs'])
